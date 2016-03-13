7064adfe8779a2c4ce7de1219e0cafbaa407a738
Andrey Yun <andrey_yun@epam.com>
2016-03-13 13:42:54 +0600

    Make use of spring-test

--- Gradle/dao/src/test/java/com/mentoring/gradle/dao/MessageDaoTest.java
+++ Gradle/dao/src/test/java/com/mentoring/gradle/dao/MessageDaoTest.java
@@ -1,9 +1,11 @@
 package com.mentoring.gradle.dao;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -12,12 +14,16 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by Andrey Yun on 28.02.2016.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DaoConfig.class})
 public class MessageDaoTest {
+
+    @Resource
+    private MessageDao messageDao;
 
     @Test
     public void shouldGetMessageFromFile() throws IOException {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(DaoConfig.class);
-        MessageDao messageDao = ctx.getBean("dao", MessageDao.class);
         String res = messageDao.retrieveMessageFromFile();
         assertThat(res, equalTo("World"));
     }
